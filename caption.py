3f7d7898bce66eac65542e298a63200e075c073b
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-24 23:45:25 +0530

    Update caption.py

--- caption.py
+++ caption.py
@@ -1,6 +1,28 @@
 from pyrogram import Client, filters
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 import io
+from flask import Flask
+import logging
+import threading
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Initialize Flask
+bot = Flask(__name__)
+
+@bot.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@bot.route('/health')
+def health_check():
+    return 'Healthy', 200
+
+def run_flask():
+    bot.run(host='0.0.0.0', port=8080)
+    
 
 # Replace with your actual credentials
 api_id = 'YOUR_API_ID'
@@ -38,4 +60,8 @@ async def handle_video(client, message):
     # Send the edited video back
     await message.reply_video(output_video_stream, caption="Here is your video with the caption at the top.")
 
-app.run()
+if __name__ == '__main__':
+    threading.Thread(target=run_flask).start()
+    
+    # Start the Pyrogram Client
+    app.run()
