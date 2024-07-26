24cb902dbdde9a7662d497c1c371a5e5be02b4a7
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-26 22:04:11 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-    HOST = os.environ.get("HOST", "")
+    API_ID = int(os.environ.get("API_ID", "25731065"))
+    API_HASH = os.environ.get("API_HASH", "be534fb5a5afd8c3308c9ca92afde672")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7213907869:AAFsNNoro8CcZSM1hkh_-a6-R41U3pqC6lA")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "sample")
+    HOST = os.environ.get("HOST", "localhost")
     IAM_HEADER = os.environ.get("IAM_HEADER", "")
     SAMPLE_VIDEO_DURATION = int(os.environ.get("SAMPLE_VIDEO_DURATION", 10))  # Default to 10 seconds
