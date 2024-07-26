eb8e4c5c0abe62bd0d60b97de807be8b753f1c30
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-26 20:46:03 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
+    HOST = os.environ.get("HOST", "")
+    IAM_HEADER = os.environ.get("IAM_HEADER", "")
+    SAMPLE_VIDEO_DURATION = int(os.environ.get("SAMPLE_VIDEO_DURATION", 10))  # Default to 10 seconds
