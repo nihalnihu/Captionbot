a930ec60853601fba2b7d0eb694f5ee3736f679f
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-26 14:31:37 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,11 +1,37 @@
 from pyrogram import Client, filters
 import asyncio
 import aiohttp
+from flask import Flask
+import subprocess
+import logging
+import threading
 
 # Replace these with your values
 API_ID = '25731065'
 API_HASH = 'be534fb5a5afd8c3308c9ca92afde672'
 BOT_TOKEN = '7213907869:AAGGYfN9m0OdUVtk-LzhEtyKJx3qVO8_DPI'
+
+
+# Initialize Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Initialize Flask
+bot = Flask(__name__)
+
+@bot.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@bot.route('/health')
+def health_check():
+    return 'Healthy', 200
+
+def run_flask():
+    bot.run(host='0.0.0.0', port=8080)
+
+
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
@@ -41,4 +67,9 @@ async def generate_preview(file_id, file_type):
             else:
                 print(f"Failed to download file: {resp.status}")
 
-app.run()
+
+if __name__ == '__main__':
+    threading.Thread(target=run_flask).start()
+    
+    # Start the Pyrogram Client
+    app.run()
