1a98f065b50eb5948940e5f0b41d1192edd504b8
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-26 20:46:31 +0530

    Create messages.py

---
+++ bot/messages.py
@@ -0,0 +1,4 @@
+class Messages:
+    SAMPLE_VIDEO_PROCESS_START = "😀 Generating Sample Video! This might take some time."
+    SAMPLE_VIDEO_PROCESS_FAILED = "😟 Sorry! Sample video generation failed possibly due to some infrastructure failure 😥."
+    SAMPLE_VIDEO_PROCESS_SUCCESS = "🤓 Sample video was generated successfully!, Now starting to upload!"
