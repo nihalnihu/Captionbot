7f044a434f49e14e0260765e54bee6486cb4f71c
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-27 07:48:02 +0530

    Update web.py

--- bot/web.py
+++ bot/web.py
@@ -9,8 +9,8 @@ def home():
     return "Bot is running"
 
 def run():
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    
 def keep_alive():
     t = Thread(target=run)
     t.start()
