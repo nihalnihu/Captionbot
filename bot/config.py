6e1f77a4c7133bed1a897995686853bbe0f5929d
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-27 07:41:19 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "25731065"))
-    API_HASH = os.environ.get("API_HASH", "be534fb5a5afd8c3308c9ca92afde672")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7213907869:AAFsNNoro8CcZSM1hkh_-a6-R41U3pqC6lA")
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
     SESSION_NAME = os.environ.get("SESSION_NAME", "my_bot")
     HOST = os.environ.get("HOST", "localhost")  # Use 'localhost' for local testing
     IAM_HEADER = os.environ.get("IAM_HEADER", "")
